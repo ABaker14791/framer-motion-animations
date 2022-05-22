c20d1929769a91ab57bfbb7faab6eeae57848eb6
Adam Baker <adam_baker@live.co.uk>
2022-05-22 19:25:50 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import "./App.css";
+import { motion } from "framer-motion";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1 className="title">Animations</h1>
+        <motion.div
+          className="square"
+          animate={{ x: "0" }}
+          initial={{ x: "-100vw" }}
+        ></motion.div>
+        <motion.p
+          className="square-text"
+          animate={{ x: "0" }}
+          initial={{ x: "100vw" }}
+          transition={{ delay: 0.4 }}
+        >
+          Animate on the x axis
+        </motion.p>
+        <div className="divider"></div>
+        <motion.div
+          className="square"
+          animate={{ y: "0" }}
+          initial={{ y: "100vh" }}
+        ></motion.div>
+        <motion.p
+          className="square-text"
+          animate={{ y: "0" }}
+          initial={{ y: "100vh" }}
+          transition={{ delay: 0.4 }}
+        >
+          Animate on the y axis
+        </motion.p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
